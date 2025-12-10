d062f813c48b9b1a7f20e487ec639db44eb4790c
benton <ben@benten.ca>
2025-12-09 18:13:47 -0800

    update playground imports

--- playground/entry.ts
+++ playground/entry.ts
@@ -1,9 +1,16 @@
-import { FreeLookComponent, InfiniteGridHelper, Transform, ActiveCameraComponent, ThreeRenderSystem, GLTFAsset } from "elysiatech/three"
-import { AmbientLight, BoxGeometry, Color, Mesh, MeshStandardMaterial, PerspectiveCamera } from "three"
-import { Engine } from "elysiatech/engine"
-import type { World } from "elysiatech/ecs"
-import { cast, mustExist } from "elysiatech/lib/asserts"
-import { AssetLoader } from "elysiatech/asset"
+import * as Three from "three"
+import {
+	Engine,
+	AssetLoader,
+	FreeLookComponent,
+	InfiniteGridHelper,
+	Transform,
+	ActiveCameraComponent,
+	GLTFAsset,
+	World,
+	cast,
+	mustExist
+} from "elysiatech/mod"
 
 const assets = await new AssetLoader({
 	dummy: new GLTFAsset("/Dummy.glb")
@@ -11,29 +18,22 @@ const assets = await new AssetLoader({
 
 const canvas = cast<HTMLCanvasElement>(mustExist(document.getElementById("viewport")))
 
-function init(world: World)
-{
-	world.addSystem(ThreeRenderSystem)
+function init(world: World) {
 	// env
 	world.addSingletonComponents(
-		new AmbientLight("white", 2),
+		new Three.AmbientLight("white", 2),
 		new InfiniteGridHelper(1, 1, undefined, 100)
 	)
-
 	// camera
 	world.createEntityWith(
 		new Transform().setPosition(0, 0, 5),
-		new PerspectiveCamera(75, devicePixelRatio, 0.1, 1000),
+		new Three.PerspectiveCamera(75, devicePixelRatio, 0.1, 1000),
 		new ActiveCameraComponent(),
 		new FreeLookComponent(),
 	);
-
-	console.log(assets.dummy.unwrap().clone())
-
 	// box
 	world.createEntityWith(
 		new Transform(),
-		// new Mesh(new BoxGeometry(1, 1, 1), new MeshStandardMaterial({ color: 0x00ff00 })),
 		assets.dummy.unwrap().clone()
 	);
 }
